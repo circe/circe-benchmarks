c04072302f4efe5d4694ae4ad6ccd85a3806c8ad
Ricardo de Souza <ricardo@icemobile.com>
2017-05-29 15:00:13 +0200

    Added Jackson definitions file.

---
+++ src/main/scala/io/circe/benchmarks/JacksonDefinitions.scala
@@ -0,0 +1,48 @@
+package io.circe.benchmarks
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.databind.JsonNode
+import org.openjdk.jmh.annotations._
+
+trait JacksonData { self: ExampleData =>
+  val mapper: ObjectMapper = new ObjectMapper()
+  mapper.registerModule(DefaultScalaModule)
+
+  @inline def encodeJackson[T](value: T): String = mapper.writeValueAsString(value)
+
+  val foosJackson: String = encodeJackson(foos)
+  val intsJackson: String = encodeJackson(ints)
+}
+
+trait JacksonEncoding { self: ExampleData =>
+  @Benchmark
+  def encodeFoosJackson: String = encodeJackson(foos)
+
+  @Benchmark
+  def encodeIntsJackson: String = encodeJackson(ints)
+}
+
+trait JacksonDecoding { self: ExampleData =>
+  @Benchmark
+  def decodeFoosJackson: Map[String, Foo] = mapper.readValue(foosJackson, classOf[Map[String, Foo]])
+
+  @Benchmark
+  def decodeIntsJackson: List[Int] = mapper.readValue(intsJackson, classOf[List[Int]])
+}
+
+trait JacksonPrinting { self: ExampleData =>
+  @Benchmark
+  def printFoosJackson: String = foosJackson.toString()
+
+  @Benchmark
+  def printIntsJackson: String = intsJackson.toString()
+}
+
+trait JacksonParsing { self: ExampleData =>
+  @Benchmark
+  def parseFoosJackson: JsonNode = mapper.readTree(foosJson)
+
+  @Benchmark
+  def parseIntsJackson: JsonNode = mapper.readTree(intsJson)
+}
