daa17c8bc6f730395ab58f2980da3258d6f7bd36
Travis Brown <travisrobertbrown@gmail.com>
2019-07-03 07:58:14 -0500

    Fix formatting that changes meaning

--- src/main/scala/io/circe/benchmarks/PlayDefinitions.scala
+++ src/main/scala/io/circe/benchmarks/PlayDefinitions.scala
@@ -5,14 +5,14 @@ import play.api.libs.functional.syntax._
 import play.api.libs.json.{ Format, JsPath, JsValue, Json, Writes }
 
 trait PlayFooInstances {
-  implicit val playFormatFoo: Format[Foo] =
+  implicit val playFormatFoo: Format[Foo] = (
     (JsPath \ "s")
       .format[String]
       .and((JsPath \ "d").format[Double])
       .and((JsPath \ "i").format[Int])
       .and((JsPath \ "l").format[Long])
       .and((JsPath \ "bs").format[List[Boolean]])
-  (Foo.apply, unlift(Foo.unapply))
+    )(Foo.apply, unlift(Foo.unapply))
 }
 
 trait PlayData { self: ExampleData =>
